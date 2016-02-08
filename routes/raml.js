31cd311c93b699e4da7c163f57c4ebd17c8325ea
Jose Miguel <josemiguel.navarro@bbva.com>
2016-02-08 10:51:11 +0100

    updated templates

--- routes/raml.js
+++ routes/raml.js
@@ -1,18 +1,10 @@
 var express = require('express');
 var router = express.Router();
 var path = require('path');
+var fs = require("fs");
 
 var raml2html = require('raml2html');
 var raml2obj = require('raml2obj');
-
-var nunjucks = require('nunjucks');
-var env = new nunjucks.Environment();
-
-env.addFilter('marked', function(str) {
-  return "MARKED";
-});
-
-var file = './raml/accounts_usa.raml';
 
 router.get('/obj/:filename', function(request, response, next) {
   var ramlFile = './raml/' + request.params.filename
@@ -27,6 +19,73 @@ router.get('/obj/:filename', function(request, response, next) {
 
 });
 
+function preprocessRamlObj(ramlObj, queryParams){
+  var apiName = queryParams.apiName;
+  var documentation = ramlObj['documentation'];
+  if (documentation === undefined)
+    documentation = []
+
+  var termsIndex = -1,
+      authIndex = -1,
+      resourceIndex = -1;
+
+  for(var index in documentation){
+    var tempTitle = documentation[index].title;
+    console.log('Index: ',index,' Title: ',tempTitle);
+    if (tempTitle.toLowerCase().indexOf('terms') > -1)
+      termsIndex = index;
+    else if (tempTitle.toLowerCase().indexOf('resources') > -1)
+      resourceIndex = index;
+    else if (tempTitle.toLowerCase().indexOf('authentication') > -1)
+      authIndex = index;
+  }
+
+  if(authIndex === -1){
+    console.log('auth')
+
+  }
+  var quickstart = queryParams.quickstart;
+  if(quickstart !== undefined){
+    var quickstartText = {
+      paystats: 'templates/documentation/quickstart/PAYSTATS.md',
+      general: 'templates/documentation/quickstart/GENERAL.md'
+    }
+    var quickText = fs.readFileSync(__dirname + "/../" + quickstartText[quickstart], 'utf8')
+    quickText = quickText.replace(/\"api-name\"/g, apiName)
+    var quickstart = {
+      title: 'Quickstart',
+      content: quickText
+    }
+    if (termsIndex > -1) {
+      documentation.splice(termsIndex, 0, quickstart)
+    } else { //no terms & conds
+      documentation.push(quickstart)
+    }
+  }
+  var termsLink = queryParams.termsLink;
+  if(termsLink !== undefined && termsLink.length > 0 && termsIndex === -1){
+    var textTerms = fs.readFileSync(__dirname + "/../" + 'templates/documentation/TERMSANDCONDS.md', 'utf8');
+    textTerms = textTerms.replace("\"api-name\"", apiName)
+    textTerms = textTerms.replace("\"terms-link\"", termsLink)
+    var terms = {
+      title: 'Terms & Conditions',
+      content: textTerms
+    }
+    documentation.push(terms)
+  }
+  if(resourceIndex === -1){
+    var textResources = fs.readFileSync(__dirname + "/../" + 'templates/documentation/RESOURCES.md', 'utf8');
+    var resources = {
+      title: 'Related resources',
+      content: textResources
+    }
+    documentation.push(resources)
+  }
+
+  ramlObj['documentation'] = documentation;
+  return ramlObj;
+}
+
 router.get('/api_market/:filename', function(request, response, next) {
   var ramlFile = './raml/' + request.params.filename
   var config = raml2html.getDefaultConfig('./templates/api_market/documentation.nunjucks', __dirname+'/..');
@@ -37,8 +96,7 @@ router.get('/api_market/:filename', function(request, response, next) {
       /**
        * Custom raml obj process
        */
-
-
+      //ramlObj = preprocessRamlObj(ramlObj, request.query)
       return config.processRamlObj(ramlObj);
     },
     postProcessHtml: config.postProcessHtml
