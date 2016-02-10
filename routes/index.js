c3730427cbe4eee9d32d0acef21fa17c1b4d668a
Jose Miguel <josemiguel.navarro@bbva.com>
2016-02-10 10:20:13 +0100

    updated templates

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,38 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'documentationRaml' });
+  var values = {
+    title: 'documentationRaml',
+    ramls: [
+      {
+        name: 'Account USA',
+        file:'accounts_usa.raml'
+      },
+      {
+        name: 'Github',
+        file:'github.raml'
+      },
+      {
+        name: 'Example',
+        file:'example.raml'
+      }
+    ],
+    options: [
+      {
+        path: '',
+        name: 'Default raml2html Template'
+      },
+      {
+        path: '/api_market',
+        name: 'API_Market Template'
+      },
+      {
+        path: '/obj',
+        name: 'Object'
+      }
+    ]
+  }
+  res.render('index', values);
 });
 
 module.exports = router;
